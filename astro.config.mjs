c876370fe5ec1260dc847ee086956aa5ce2c1207
Peter Krivosudský <petokrivo@gmail.com>
2025-10-13 17:52:21 +0200

    feat: nové stránky + obrázky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      // Použiť Tailwind config
-      configFile: './tailwind.config.cjs',
-      // Aplikovať base styles
-      applyBaseStyles: true
-    })
-  ],
-  output: 'static',
-  server: {
-    port: 4321,
-    host: true
-  }
+  integrations: [tailwind()],
 });
